b832361d49e83f3d09760399632924e2e3475911
Søren Magnusson <soren@magnussons.dk>
2017-12-11 07:58:23 +0100

    Pin som konstant definition

--- projekter/auto cookie cliker/build.ino
+++ projekter/auto cookie cliker/build.ino
@@ -1,12 +1,14 @@
+#define MOUSE_PIN 3
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode (13,OUTPUT);
+  pinMode (MOUSE_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13,HIGH);
+  digitalWrite(MOUSE_PIN, HIGH);
   delay(100);
-  digitalWrite(13,LOW);
+  digitalWrite(MOUSE_PIN, LOW);
   delay(100);
 }
