a0f3896a06b0f3d616c1eae29399a1fbfa7a025d
Søren Magnusson <smag@tec.dk>
2018-02-05 09:12:24 +0100

    Nu med test af digitalt input

--- projekter/persienne/h206-analog/h206-analog.ino
+++ projekter/persienne/h206-analog/h206-analog.ino
@@ -2,19 +2,47 @@
 int PinOut = A0; //Set assign digital pin 2
 int myDelay = 50;
 int val;
- 
+
+int dPin1 = 2;
+int dPin2 = 3;
+
+
 void setup()
 {
 Serial.begin(9600); //Set Serial Communication
 pinMode(PinOut, INPUT); //Set as INPUT
 Serial.println("14CORE | Opto Interrupter Analog Out Test Code");
 Serial.println("-----------------------------------------------");
+
+pinMode(dPin1, INPUT);
+pinMode(dPin2, INPUT);
 }
  
 void loop(){
- val = analogRead(PinOut);
- //Serial.print("Sensor Value:");
- Serial.println(val);
- delay(myDelay);
+  //val = analogRead(PinOut);
+  val = digitalRead(PinOut);
+  //Serial.print("Sensor Value:");
+  
+  //Serial.print("X: ");
+  //Serial.print(val*2);
+  //Serial.print(", ");
+  
+  Serial.print("Direct opto: ");
+  Serial.print(val);
+  
+  int val1 = digitalRead(dPin1);
+  int val2 = digitalRead(dPin2);
+
+  Serial.print(", ");
+  Serial.print("d1: ");
+  Serial.print(val1);
+
+  Serial.print(", ");
+  Serial.print("d2: ");
+  Serial.print(val2);
+
+  Serial.println();
+  
+  delay(myDelay);
 }
 
